e586d69f3b1bc57571aed690ddf8d383cd2fcc94
Rafael <rafaelhenriquebcm@gmail.com>
2025-01-17 16:34:12 -0300

    Refs: master - develop - Adicionando 2 campos na view show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.admin')
 @section('content')
-    <div class="container mt-5">
         <h1 class="text-center mb-4">Show User</h1>
         <div class="card w-50 mx-auto shadow">
             <div class="card-body">
@@ -16,10 +15,17 @@
                     <div><strong>Email</strong></div>
                     <div class=" text-nowrap">{{ $user->email }}</div>
                 </div>
+                <div class="row mb-3">
+                    <div><strong>Cadatrado</strong></div>
+                    <div class=" text-nowrap">{{ \Carbon\Carbon::parse($user->created_at)->format('d/m/Y H:i:s') }}</div>
+                </div>
+                <div class="row mb-3">
+                    <div><strong>Editado</strong></div>
+                    <div class=" text-nowrap">{{ \Carbon\Carbon::parse($user->updated_at)->format('d/m/Y H:i:s') }}</div>
+                </div>
                 <div class="mt-4">
                     <a href="{{ route('users.index') }}" class="btn btn-secondary">Back</a>
                 </div>
             </div>
         </div>
-    </div>
 @endsection()
