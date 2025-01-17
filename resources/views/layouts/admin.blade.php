a762211be0a1e9e2cbdbbe08d1eb4143c1132f70
Rafael <rafaelhenriquebcm@gmail.com>
2025-01-17 12:50:07 -0300

    [Refs: master - develop - modificando o layout admin]

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Basic Crud</title>
 </head>
+
 <body>
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span  href="{{ route('users.index') }}" class="fs-4">Basic Crud</span>
-        </a>
-    </header>
+    <nav class="navbar bg-primary border-body mb-3" data-bs-theme="dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Simple CRUD</a>
+        </div>
+    </nav> 
     <x-alert />
     @yield('content')
 
@@ -20,10 +21,12 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-8 d-flex align-items-center">
-                    <span class="mb-0">© Copyright 2025 Todo conteúdo do site pode ser reproduzido, desde que citada a fonte.</span>
+                    <span class="mb-0">© Copyright 2025 Todo conteúdo do site pode ser reproduzido, desde que citada a
+                        fonte.</span>
                 </div>
             </div>
         </div>
     </footer>
 </body>
+
 </html>
